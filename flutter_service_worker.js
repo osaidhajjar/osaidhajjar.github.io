bec2b2b24fb4b368b08780a214369bb39208616b
Osaid Hajjar <ofhajjar@hotmail.com>
2021-01-23 02:28:38 +0300

    imporvements

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -82,8 +82,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "d499484966d111eee6ffef7f56a8939c",
-"/": "d499484966d111eee6ffef7f56a8939c",
+"index.html": "21ae888ed762b7b7d70b1f7cf274172c",
+"/": "21ae888ed762b7b7d70b1f7cf274172c",
 "main.dart.js": "0f88b738bcbbdf2dce480221654da15b",
 "manifest.json": "f35bd00acc6350598b6e1e2590f088e8",
 "version.json": "37da58b8142ff9cd5f0f2c9db59f2fcf"
